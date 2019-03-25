db7e1630d60b25e53fca4c13774ce5e270d4c576
Aman Thapar <10161022+amanthapar@users.noreply.github.com>
2019-03-25 16:33:01 -0400

    QR sanner can add friends from DB (#69)

--- client/components/FriendsList.js
+++ client/components/FriendsList.js
@@ -21,6 +21,7 @@ class FriendsList extends Component {
   }
 
   toggle = () => this.setState({checked: !this.state.checked})
+
   handleAddEmail = email => {
     this.props.addFriendThunk(this.props.user.email, email)
   }
@@ -160,7 +161,7 @@ class FriendsList extends Component {
           {this.state.showform[0] && <Button type="submit">Submit</Button>}
         </Form>
         <Button onClick={this.toggle}>Toggle QR Scanner</Button>
-        {this.state.checked ? <Readqr /> : ''}
+        {this.state.checked ? <Readqr toggle={this.toggle} /> : ''}
         {this.props.errorMsg && (
           <Message error style={{width: '80%', maxWidth: '450px'}}>
             {this.props.errorMsg}
