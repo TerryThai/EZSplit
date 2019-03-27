5543f36de7850704f6005118c2e1d53f1fa12735
Aman Thapar <10161022+amanthapar@users.noreply.github.com>
2019-03-27 17:58:43 -0400

    updating email base url

--- server/email/sendLinkTemplate.js
+++ server/email/sendLinkTemplate.js
@@ -5,7 +5,7 @@ module.exports = email => {
     <div style='text-align: center;'>
       <h3> Hello! Thanks for using EzSplit! </h3>
       <p>${email.body}<p>
-      <div><a href="http://localhost:3000/editReceipt/${
+      <div><a href="https://ezsplit.herokuapp.com/editReceipt/${
         email.receiptId
       }">Click here to open table</a></div>
     </div>
--- server/email/userAmountsEmail.js
+++ server/email/userAmountsEmail.js
@@ -5,7 +5,7 @@ module.exports = email => {
     <div style='text-align: center;'>
       <h4> Thanks for using ezsplit! </h4>
       <p>Group :${email.groupName}<p>
-      <div><a href="http://localhost:3000/editReceipt/${
+      <div><a href="https://ezsplit.herokuapp.com/editReceipt/${
         email.receiptId
       }">Click here if you need reference to the table</a></div>
       <p>${email.body}<p>
